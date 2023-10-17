a85b68c96899a038ed3bc9216496326218f323f7
Areefa0504 <areefa786in@gmail.com>
2023-10-18 00:08:27 +0100

    Data_Transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,11 +7,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
-
-from src.components.model_trainer import ModelTrainerConfig
-from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -33,6 +28,7 @@ class DataIngestion:
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
 
             logging.info("Train test split initiated")
+            
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
@@ -53,8 +49,4 @@ if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation=DataTransformation()
-    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
-
-    modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+  
--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -1,4 +1,3 @@
-import os
 import sys
 from dataclasses import dataclass
 
@@ -11,12 +10,7 @@ from sklearn.preprocessing import OneHotEncoder,StandardScaler
 
 from src.exception import CustomException
 from src.logger import logging
-
-from sklearn.model_selection import train_test_split
-from dataclasses import dataclass
-
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+import os
 
 from src.utils import save_object
 
@@ -128,8 +122,3 @@ class DataTransformation:
             )
         except Exception as e:
             raise CustomException(e,sys)
-        
-if __name__=="__main__":
-    obj=DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
-    data_transformation.initiate_data_transformation(train_data,test_data)
